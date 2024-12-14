d5d14eda1502f9d03ed6aa239afb33be988cf03a
Voronov Igor <voronov.i.v@mail.ru>
2024-12-14 13:10:31 +0300

    add empty line and task card

--- app/src/main/java/com/example/simbirsoftdayplanner/presentation/mainscreen/MainActivity.kt
+++ app/src/main/java/com/example/simbirsoftdayplanner/presentation/mainscreen/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.simbirsoftdayplanner.presentation.mainscreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,11 +23,18 @@ import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.simbirsoftdayplanner.domain.Task
@@ -39,6 +48,13 @@ class MainActivity : ComponentActivity() {
 
 //        val vm = ViewModelProvider(this)[MainViewModel::class.java]
 
+
+        val mockMap = mapOf(
+            Pair(7, Task.mock()),
+            Pair(11, Task.mock()),
+            Pair(18, Task.mock()),
+        )
+
         setContent {
             Column(
                 modifier = Modifier
@@ -47,14 +63,11 @@ class MainActivity : ComponentActivity() {
             ) {
                 BestDatePickerEver()
 
-                DayTable()
+                DayTable(mockMap)
             }
 
             val task = Task.mock()
 
-//            val taskList = vm.getTasksList()
-
-//
 
 //            BottomPanelScaffold(modifier = Modifier, content = {
 ////                ActionButton(vm.editTask(), "Изменить")
@@ -65,16 +78,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun TaskList(taskList: List<Task>) {
-    LazyColumn() {
-
-        taskList
-    }
-}
 
 @Composable
-private fun DayTable() {
+private fun DayTable(taskMap: Map<Int, Task>) {
     Column(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
@@ -83,50 +89,80 @@ private fun DayTable() {
             .padding(8.dp)
     ) {
         for (i in 0..23) {
-            Box(
-                modifier = Modifier
-                    .padding(vertical = 2.dp)
-                    .clip(Shapes().extraSmall)
-                    .fillMaxWidth()
-                    .background(Colors.ButtonBackground)
-            ) {
-                Text(
-                    modifier = Modifier.padding(2.dp),
-                    text = "$i:00  -  ${i + 1}:00",
-                    color = Colors.Text
-                )
+            if (i !in taskMap.keys) {
+                EmptyTableLine(i = i)
+            } else {
+                TaskTableLine(taskMap[i] ?: Task.mock())
             }
         }
     }
 }
 
 @Composable
-private fun Task(task: Task) {
+private fun TaskTableLine(task: Task) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .padding(vertical = 2.dp)
+            .clip(Shapes().extraSmall)
+            .fillMaxWidth()
             .background(Colors.ButtonBackground)
     ) {
         Column(
             modifier = Modifier
+                .padding(12.dp)
         ) {
             Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
+                    modifier = Modifier.padding(2.dp),
                     text = task.name,
+                    fontSize = 12.sp,
                     color = Colors.Text,
-                )
+
+                    )
                 Text(
-                    text = task.dateFinish.toString(),
+                    modifier = Modifier.padding(2.dp),
+                    text = "${task.dateStart} - ${task.dateFinish}",
+                    fontSize = 12.sp,
                     color = Colors.Time,
                 )
             }
             Text(
+                modifier = Modifier.padding(2.dp),
                 text = task.description,
+                fontSize = 8.sp,
                 color = Colors.Text,
-            )
+
+                )
         }
+    }
+}
+
+@Composable
+private fun EmptyTableLine(i: Int) {
+    val isSelected = remember{mutableStateOf(false)}
+
+    Box(
+        modifier = Modifier
+            .padding(vertical = 2.dp)
+            .clip(Shapes().extraSmall)
+            .fillMaxWidth()
+            .background(Colors.ButtonBackground)
+            .clickable {
+                isSelected.value != isSelected.value
+            }, // add click action,
+        contentAlignment = Alignment.CenterEnd,
+    ) {
+        Text(
+            modifier = Modifier.padding(vertical = 2.dp, horizontal = 8.dp),
+            text = "$i:00",
+            fontSize = 8.sp,
+            color = if (isSelected.value) Colors.chosenDate else Colors.Time,
+        )
+
     }
 }
 
@@ -148,7 +184,7 @@ fun BottomPanelScaffold(
 
 @Composable
 private fun ActionButton(
-    onClick: Unit,
+    onClick: () -> Unit,
     text: String
 ) {
     Button(
@@ -163,7 +199,8 @@ private fun ActionButton(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun BestDatePickerEver() {
-    DatePicker(state = DatePickerState(Locale.getDefault()),
+    DatePicker(
+        state = DatePickerState(locale = Locale.getDefault()),
         title = null,
         headline = null,
         showModeToggle = false,
